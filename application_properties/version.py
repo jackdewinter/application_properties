73e4a2038a07df13381b0568556e1f2b62c2c902
Jack De Winter <jack.de.winter@outlook.com>
2025-06-15 16:30:30 -0700

    https://github.com/jackdewinter/application_properties/issues/271

--- application_properties/version.py
+++ application_properties/version.py
@@ -2,7 +2,7 @@
 Library version information.
 """
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 __project_name__ = "application_properties"
 __description__ = (
     "A simple, easy to use, unified manner of accessing program properties."
